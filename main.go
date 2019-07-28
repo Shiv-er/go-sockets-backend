1071f2424775ced7ee8b75c94ecb22e0a4cd70da
shiv-er <shivh623@gmail.com>
2019-07-28 11:27:27 +0530

    code revamp in main

--- main.go
+++ main.go
@@ -8,23 +8,20 @@ import (
 )
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	ws, err := websocket.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(w, "%+V\n", err)
-	}
-
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+    ws, err := websocket.Upgrade(w, r)
+    if err != nil {
+        fmt.Fprintf(w, "%+V\n", err)
+    }
+    go websocket.Writer(ws)
+    websocket.Reader(ws)
 }
 
 func setupRoutes() {
-	http.HandleFunc("/ws", serveWs)
+    http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
-	fmt.Println("Chat App v0.01")
-	setupRoutes()
-	http.ListenAndServe(":8080", nil)
+    fmt.Println("Distributed Chat App v0.01")
+    setupRoutes()
+    http.ListenAndServe(":8080", nil)
 }
